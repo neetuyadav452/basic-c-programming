df2a25286d382ea574452bc569a964ae00c3b713
unknown <neetuyadav1026420@gmail.com>
2025-04-01 13:06:59 +0530

    learned

---
+++ linklist.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node*next;
+};
+void traversal(struct node*ptr){
+    while (ptr != NULL) {
+        printf("elements : %d\n",ptr->data);
+      ptr = ptr->next;
+    }
+    void insertatbegin(int data){
+        struct node * step = (struct node*)malloc(sizeof(struct node));
+        step->next = head;
+        step->data = data;
+ head = step;
+    }
+}
+int main(){
+    //memoery allocate in stack
+    struct node *first; 
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+    struct node *fifth;
+    // memory allocate in heap
+    first = (struct node*)malloc(sizeof(struct node));
+    second = (struct node*)malloc(sizeof(struct node));
+    third = (struct node*)malloc(sizeof(struct node));
+    fourth = (struct node*)malloc(sizeof(struct node));
+    fifth = (struct node*)malloc(sizeof(struct node));
+    first->data = 7;
+    first->next = second;
+    second->data = 14;
+    second->next = third;
+    third->data = 21;
+    third->next = fourth;
+    fourth->data =28;
+    fourth->next = fifth;
+    fifth->data = 35;
+    fifth->next = NULL;
+traversal(first);
+insertatbegin(42);
+    return 0;
+    }
